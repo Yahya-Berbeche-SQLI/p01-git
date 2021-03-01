2f06f4a5c5ed9c8e1dd5d7ccd6a15b25a0c388a0
Yahya Berbeche <yberbeche@sqli.com>
2021-03-01 15:32:50 +0100

    my third commit

--- src/org/sqli/actions/Main.java
+++ src/org/sqli/actions/Main.java
@@ -3,8 +3,5 @@ package org.sqli.actions;
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello World, This is me Yahya");
-        for (int i = 0; i < 10; i++) {
-            System.out.println("Hello : "+ i);
-        }
     }
 }
